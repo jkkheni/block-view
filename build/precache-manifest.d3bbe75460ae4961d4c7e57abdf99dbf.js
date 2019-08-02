bb56b478fb4d46c647fcef7405db132fe507ab73
ryaan infotech <ryaaninfotech@gmail.com>
2019-08-02 21:13:43 +0530

    remove build folder

---
+++ build/precache-manifest.d3bbe75460ae4961d4c7e57abdf99dbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c986c3a3108191236e4eb06f413197",
+    "url": "/block-view/index.html"
+  },
+  {
+    "revision": "f843b65c998c0d663eb5",
+    "url": "/block-view/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9f60f56eb85dfbcc77c2",
+    "url": "/block-view/static/js/2.24ba59b7.chunk.js"
+  },
+  {
+    "revision": "f843b65c998c0d663eb5",
+    "url": "/block-view/static/js/main.63e973cb.chunk.js"
+  },
+  {
+    "revision": "e2e2fdf3f7df9671ee59",
+    "url": "/block-view/static/js/runtime~main.3f7c8455.js"
+  },
+  {
+    "revision": "b555c2fd52beb327daf7d4b753e68f9f",
+    "url": "/block-view/static/media/desert.b555c2fd.png"
+  },
+  {
+    "revision": "7b2d9976b6855a3f97a593a4b8ab8f44",
+    "url": "/block-view/static/media/list.7b2d9976.png"
+  },
+  {
+    "revision": "4aae9d50683dac8e8f4416e991d9d174",
+    "url": "/block-view/static/media/tip.4aae9d50.png"
+  }
+]);
